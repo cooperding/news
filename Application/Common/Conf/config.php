db68d7f027204cd7569b81a914f5a85889936eb4
dingcms <lookcms@gnail.com>
2014-03-28 11:41:48 +0800

    整理模块，启用语言侦测

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -5,7 +5,7 @@ return array(
     'SESSION_AUTO_START' => true, //是否开启session
     'LOAD_EXT_CONFIG' => 'db', // 加载扩展配置文件
     'MODULE_DENY_LIST' => array('Common', 'Runtime'),
-    'MODULE_ALLOW_LIST' => array('Home', 'Admin', 'User', 'Shop', 'Food', 'Api'), // 允许访问的模块列表
+    'MODULE_ALLOW_LIST' => array('Home', 'Admin', 'User', 'Api'), // 允许访问的模块列表
     'DEFAULT_MODULE' => 'Home', //默认模块
     'URL_CASE_INSENSITIVE' => false, // 默认false 表示URL区分大小写 true则表示不区分大小写
     'MULTI_MODULE' => true, // true开启多模块访问，false关闭多模块访问
@@ -15,7 +15,7 @@ return array(
     'URL_DENY_SUFFIX' => 'ico|png|gif|jpg', // URL禁止访问的后缀设置
     'READ_DATA_MAP' => true, //字段映射,在数据获取的时候自动处理的话
     'LANG_SWITCH_ON' => true, // 开启语言包功能
-    'LANG_AUTO_DETECT' => false, // 自动侦测语言 开启多语言功能后有效
+    'LANG_AUTO_DETECT' => true, // 自动侦测语言 开启多语言功能后有效
     'LANG_LIST' => 'en-us,zh-cn', // 允许切换的语言列表 用逗号分隔
     'VAR_LANGUAGE' => 'l', // 默认语言切换变量
     'TAGLIB_BUILD_IN' => 'cx,html',
