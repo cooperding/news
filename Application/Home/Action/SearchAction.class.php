743e518e832418370a14997904040b76708c21a9
dingcms <dingjicai@dingjicaideMacBook-Pro.local>
2015-04-07 20:44:25 +0800

    实现搜索功能

--- Application/Home/Action/SearchAction.class.php
+++ Application/Home/Action/SearchAction.class.php
@@ -10,47 +10,47 @@
  * @package  Controller
  * @todo 完善更多方法
  */
+
 namespace Home\Action;
+
 use Think\Action;
+
 class SearchAction extends BasehomeAction {
 
     public function index() {
         //分析：根据提供的关键词查询title或者扩展内容表（暂定）,同时需要查询的表有分类表。
         $words = addslashes($_GET['words']);
-        if (!$words) {
-            echo '错误'; //跳转到错误页面
-            exit;
-        }
-        import('ORG.Util.Page'); // 导入分页类
+        /*
+          if (!$words) {
+          echo '错误'; //跳转到错误页面
+          exit;
+          }
+         * 
+         */
+
         $t = M('Title');
         $ns = M('NewsSort');
         $condition['t.title'] = array('like', '%' . $words . '%');
         $condition['t.keywords'] = array('like', '%' . $words . '%');
         $condition['_logic'] = 'OR';
         $count = $t->Table(C('DB_PREFIX') . 'title t')->where($condition)->count();
-        $Page = new Page($count, 10); // 实例化分页类 传入总记录数和每页显示的记录数
-        $show = $Page->show(); // 分页显示输出
+        $page = new \Org\Util\QiuyunPage($count, 10); // 实例化分页类 传入总记录数和每页显示的记录数
+        $show = $page->show(); // 分页显示输出
         // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
-        $list = $t->field(array('t.*', 'ns.text' => 'sortname', 'ms.emark'))
+        $list = $t
+                ->field(array('t.id,t.sort_id,t.title,t.subtitle,t.titlepic,t.views,t.keywords,t.description,t.addtime,t.updatetime,t.num_top,t.num_beat,t.num_comment,ns.text'))
                 ->Table(C('DB_PREFIX') . 'title t')
                 ->join(C('DB_PREFIX') . 'news_sort ns ON ns.id = t.sort_id')
-                ->join(C('DB_PREFIX') . 'model_sort ms ON ms.id = ns.model_id')
-                ->where($condition)->limit($Page->firstRow . ',' . $Page->listRows)
+                ->where($condition)->limit($page->firstRow . ',' . $page->listRows)
                 ->select();
-        foreach ($list as $k => $v) {
-            $m = M(ucfirst(C('DB_ADD_PREFIX') . $v['emark']));
-            $mdata = $m->where('title_id=' . $v['id'])->find();
-            if ($mdata) {
-                $list[$k] = array_merge($list[$k], $mdata);
-            }
-        }
-        $this->assign('dogocms', $list); // 赋值数据集
+        $skin = $this->getSkin(); //获取前台主题皮肤名称
+        $this->assign('list', $list); // 赋值数据集
         $this->assign('words', $words);
         $this->assign('page', $show); // 赋值分页输出
         $this->assign('title', $one_data['text']);
         $this->assign('keywords', $one_data['keywords']);
         $this->assign('description', $one_data['description']);
-        $this->display(C('TPL_NAME') . 'search');
+        $this->theme($skin)->display(C('TPL_NAME') . 'search');
     }
 
 }
