3765aac1d376587ca0af9323b8cda37b1d6b6a12
dingcms <qiuyuncode@163.com>
2015-08-22 22:01:25 +0800

    1234567

--- Application/Document/Action/IndexAction.class.php
+++ Application/Document/Action/IndexAction.class.php
@@ -19,21 +19,57 @@ use Think\Action;
 class IndexAction extends BasedocAction {
 
     public function index() {
-        header("Content-type:text/html;charset=utf-8");
         $m = M('DocumentList');
         $id = intval(trim(I('get.id')));
-        if(!empty($id)){//文档id不为空
-            
+        $sid = intval(trim(I('get.sid')));
+        /* 分析：
+         * 分类(sid)与文档(id)同时存在时，优先级为id，同时sid无效，新的sid值由id查询得出
+         * 分类sid存在，ID不存在使用sid获取数据
+         * 分类sid不存在，ID存在，sid值由id查询得出
+         * 分类sid，ID都不存在提示新的页面信息
+         */
+        if (!empty($id)) {//文档id不为空
+            $condition['id'] = array('eq', $id);
+            $condition['status'] = array('eq', '20');
+            $data = $m->where($condition)->find();
+            if ($data) {
+                $sortid = $data['sort_id'];
+            }
+        } else {//id为空，sid不为空或者为空操作
+            if (!empty($sid)) {//分类sid不为空
+                $sortid = $sid;
+            } else {//分类sid为空
+                $sortid = '';
+            }
         }
-        $sort_id = intval(trim(I('get.sort_id')));
-        $sort_id = 7;
-        $condition['sort_id'] = array('eq',$sort_id);
-        $condition['status'] = array('eq','20');
-        $list = $m->where($condition)->select();
-        
+        $list = $this->getSortList($sortid);
+        $this->assign('data', $data);
+        $this->assign('list', $list);
+        $this->assign('title', $data['text']);
         $skin = $this->skin; //获取前台主题皮肤名称
         $tpl_home = $this->tpl_home; //获取主题皮肤模板名称
         $this->theme($skin)->display($tpl_home . 'index');
+    }
+
+    /*
+     * getSortList
+     * 通过分类ID获取，所有该分类下的信息
+     * $sid int 分类ID
+     * return array();
+     */
+
+    public function getSortList($sid) {
+        if (!empty($sid)) {//存在获取当前分类信息
+            $m = M('DocumentList');
+            $condition['sort_id'] = array('eq', $sid);
+            $condition['status'] = array('eq', '20');
+            
+        } else {//不存在，获取所有分类信息
+            $m = M('DocumentSort');
+            $condition['status'] = array('eq', '20');
+        }
+        $list = $m->where($condition)->select();
+        return $list;
     }
 
 }
